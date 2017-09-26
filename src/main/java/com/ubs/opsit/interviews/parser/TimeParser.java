33be62d6c320827a521b176926e65ba6f84ca4dc
swapnilnarvekar <swapnil.narvekar@gmail.com>
2017-09-26 16:50:17 +0530

    Added interface - TimeParser

---
+++ src/main/java/com/ubs/opsit/interviews/parser/TimeParser.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ubs.opsit.interviews.parser;
+
+/**
+ * This is an interface which can be used to parse a given time
+ * 
+ * @author Swapnil A. Narvekar
+ *
+ */
+public interface TimeParser {
+
+	String parseTime(String aGivenTime);
+}
