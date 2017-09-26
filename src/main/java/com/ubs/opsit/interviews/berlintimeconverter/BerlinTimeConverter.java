ee99d024f6b5357fb57e9b1bed63ac48478bd6d0
Swapnil Narvekar <swapnil.narvekar@gmail.com>
2017-09-26 12:21:48 +0530

    Added implementation class i.e. BerlinTimeValidator to perform validation on a given time specifically for Berlin Clock

--- src/main/java/com/ubs/opsit/interviews/berlintimeconverter/BerlinTimeConverter.java
+++ src/main/java/com/ubs/opsit/interviews/berlintimeconverter/BerlinTimeConverter.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.ubs.opsit.interviews.TimeConverter;
 import com.ubs.opsit.interviews.validator.TimeValidator;
 import com.ubs.opsit.interviews.validator.berlintimevalidator.BerlinTimeValidator;
 
 /**
- * The Class BerlinTimeConverter.
+ * The Class : <b>BerlinTimeConverter</b><br>
+ * Description :This class is responsible for converting a given time(hh:mm:ss) to 'Berlin' time
+ * It implements the interface - TimeConverter 
  *
- * @author Narya
+ * @author Swapnil A. Narvekar
  */
 public class BerlinTimeConverter implements TimeConverter{
 
+	private final static Logger LOG = Logger.getLogger(BerlinTimeConverter.class);
 	private TimeValidator timeValidator;
 	
 	
@@ -25,15 +30,38 @@ public class BerlinTimeConverter implements TimeConverter{
 		timeValidator = new BerlinTimeValidator();
 	}
 	
-	/* (non-Javadoc)
-	 * @see com.ubs.opsit.interviews.TimeConverter#convertTime(java.lang.String)
+	/**
+	 * This method converts a given time (hh:mm:ss) into 'Berlin' time in the form of String -
+	 *  <br> for e.g : if time is 13:17:01 <br>
+	 *	Then the clock should look like<br>
+	 *  O<br>
+	 *  RROO<br>
+	 *  RRRO<br>
+	 *  YYROOOOOOOO<br>
+	 *  YYOO<br>
+	 * 
 	 */
 	@Override
 	public String convertTime(String aTime) {
+		LOG.info("a given time is input as - " + aTime);
 		
 		timeValidator.validateGivenTime(aTime);
+		LOG.info("a given time is validated successfully! ");
 		
 		String[] timeComponents = aTime.split(":");
+		String resultString = berlinTimeParser(timeComponents);
+		LOG.info("a given time is converted in 'Berlin Time'successfully ");
+		
+		
+		return resultString;
+	}
+
+	/**
+	 * @param timeComponents
+	 * @return
+	 */
+	private String berlinTimeParser(String[] timeComponents) {
+		LOG.debug("timeComponents : " + Arrays.toString(timeComponents));
 		List<String> resultComponents = new ArrayList<>();
 		resultComponents.add(computeSeconds(Integer.parseInt(timeComponents[2])));
 		resultComponents.add(computeHours(Integer.parseInt(timeComponents[0])));
@@ -43,27 +71,29 @@ public class BerlinTimeConverter implements TimeConverter{
 		for(String resulComponent : resultComponents){
 			formattedString.append(resulComponent).append("\r\n");
 		}
-		String resultString  = formattedString.substring(0, formattedString.lastIndexOf("\r"));//( , formattedString.length()-1,"");
-		
-		return resultString;
+		String result = formattedString.substring(0, formattedString.lastIndexOf("\r"));
+		LOG.debug("berlin time parsed as : " + result);
+		return result;
 	}
 
 	
 	/**
-	 * Compute minutes.
+	 * Compute minutes by taking a minute component (mm) as an argument and returns a string  
 	 *
 	 * @param numberOfMinutes the number of minutes
 	 * @return the string
 	 */
 	private String computeMinutes(int numberOfMinutes) {
+		LOG.debug("numberOfMinutes : "+ numberOfMinutes);
 		int numberOfMinutesInTopRow = numberOfMinutes / 5;
 		int numberOfMinutesInBottomRow = numberOfMinutes % 5;
 		
 		String topRow = getTopRowForMinutes(numberOfMinutesInTopRow);
 		String bottomRow = getBottomRowForMinutes(numberOfMinutesInBottomRow);
 		
-		
-		return topRow+"\r\n"+bottomRow;
+		String minutes = topRow+"\r\n"+bottomRow; 
+		LOG.debug("minutes are constructed as : "+minutes);
+		return minutes;
 	}
 
 	/**
@@ -73,16 +103,22 @@ public class BerlinTimeConverter implements TimeConverter{
 	 * @return the top row for minutes
 	 */
 	private String getTopRowForMinutes(int numberOfMinutesInTopRow) {
+		LOG.debug("numberOfMinutesInTopRow : "+ numberOfMinutesInTopRow);
+		
+		//it represents a series of bulbs for minutes and all are in OFF state
 		String[] rows = new String[]{"O","O","O","O","O","O","O","O","O","O","O"};
+		
     	for(int i=0;i<numberOfMinutesInTopRow; i++){
     		if( (i != 0) && (i+1)%3 == 0){
-    			rows[i] = "R";
+    			rows[i] = "R"; //switch on a bulb with RED color
     		}else{
-    			rows[i] = "Y";
+    			rows[i] = "Y"; //switch on a bulb with YELLOW color
     		}
     	}
     	
-		return Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	String result = Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	LOG.debug("Minutes for top row are constructed as - " + result);
+		return result; 
 	}
 
 	/**
@@ -92,27 +128,36 @@ public class BerlinTimeConverter implements TimeConverter{
 	 * @return the bottom row for minutes
 	 */
 	private String getBottomRowForMinutes(int numberOfMinutesInBottomRow) {
+		LOG.debug("numberOfMinutesInBottomRow : " + numberOfMinutesInBottomRow);
+		
+		//it represents a series of bulbs for minutes and all are in OFF state
 		String[] rows = new String[]{"O","O","O","O"};
     	for(int i=0;i<numberOfMinutesInBottomRow; i++){
-    		rows[i] = "Y";
+    		rows[i] = "Y"; //switch on a bulb with YELLOW color
     	}
-    	return Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	
+    	String result = Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	LOG.debug("Minutes for bottom row are constructed as - " + result);
+    	return result;
 	}
 
 	/**
-	 * Compute hours.
+	 * Compute minutes by taking a hour component (hh) as an argument and returns a string.
 	 *
 	 * @param numberOfHours the number of hours
 	 * @return the string
 	 */
 	private String computeHours(int numberOfHours) {
+		LOG.debug("numberOfHours : " + numberOfHours);
 		int numberOfHoursInTopRow = numberOfHours / 5;
 		int numberOfHoursInBottomRow = numberOfHours % 5;
 		
 		String topRow = getTopRowForHours(numberOfHoursInTopRow);
 		String bottomRow = getBottomRowForHours(numberOfHoursInBottomRow);
 		
-		return topRow +"\r\n"+bottomRow;
+		String result = topRow +"\r\n"+bottomRow;
+		LOG.debug("Hour component is constructed as : " + result);
+		return result;
 	}
 
 	/**
@@ -122,11 +167,17 @@ public class BerlinTimeConverter implements TimeConverter{
 	 * @return the bottom row for hours
 	 */
 	private String getBottomRowForHours(int numberOfHoursInBottomRow) {
+		LOG.debug("numberOfHoursInBottomRow : " + numberOfHoursInBottomRow);
+		
+		//it represents a series of bulbs for hours and all are in OFF state
 		String[] rows = new String[]{"O","O","O","O"};
     	for(int i=0;i < numberOfHoursInBottomRow ;i++){
     		rows[i] = "R";
     	}
-    	return Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	
+    	String result = Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	LOG.debug("Hours for bottom row are constructed as - " + result);
+    	return result;
 	}
 
 	/**
@@ -136,16 +187,21 @@ public class BerlinTimeConverter implements TimeConverter{
 	 * @return the top row for hours
 	 */
 	private String getTopRowForHours(int numberOfHoursInTopRow) {
+		LOG.debug("numberOfHoursInTopRow : " + numberOfHoursInTopRow);
+		
+		//it represents a series of bulbs for hours and all are in OFF state
 		String[] rows = new String[]{"O","O","O","O"};
     	for(int i=0;i<numberOfHoursInTopRow;i++){
     		rows[i] = "R";
     	}
     	
-		return Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	String result = Arrays.toString(rows).replace(",", "").replace(" ", "").replace("[","").replace("]", "");
+    	LOG.debug("Hours for top row are constructed as - " + result);
+		return result;
 	}
 
 	/**
-	 * Compute seconds.
+	 * Compute minutes by taking a seconds component (ss) as an argument and returns a string.
 	 *
 	 * @param numberOfSeconds the number of seconds
 	 * @return the string
@@ -161,10 +217,15 @@ public class BerlinTimeConverter implements TimeConverter{
 	 * @return the row for seconds
 	 */
 	private String getRowForSeconds(int numberOfSeconds){
+		LOG.debug("numberOfSeconds : " + numberOfSeconds);
+		String result = null;
 		if(numberOfSeconds %2 == 0){
-			return "Y";
+			result = "Y";
+		}else{
+			result = "O";
 		}
+		LOG.debug("numberOfSeconds represented as : " + result);
+		return result;
 		
-		return "O";
 	}
 }
