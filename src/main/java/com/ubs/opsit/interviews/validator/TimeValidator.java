ba22917dc4c54930be520385b76e67384971d740
Swapnil Narvekar <swapnil.narvekar@gmail.com>
2017-09-26 12:20:32 +0530

    Added an interface - TimeValidator to perform validation on a given time

---
+++ src/main/java/com/ubs/opsit/interviews/validator/TimeValidator.java
@@ -0,0 +1,16 @@
+package com.ubs.opsit.interviews.validator;
+
+/**
+ * The Interface TimeValidator.
+ * 
+ * @author Swapnil A. Narvekar
+ */
+public interface TimeValidator {
+
+	/**
+	 * Validate given time.
+	 *
+	 * @param aTime the a time
+	 */
+	void validateGivenTime(String aTime);
+}
